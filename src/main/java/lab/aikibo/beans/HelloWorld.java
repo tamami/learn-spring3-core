9a8d11ef9b0de7b359b5f0fb02903c99f4b684d9
tamami <tamami.oka@gmail.com>
2016-08-02 15:50:40 +0700

    init commit. fixed

---
+++ src/main/java/lab/aikibo/beans/HelloWorld.java
@@ -0,0 +1,15 @@
+package lab.aikibo.beans;
+
+public class HelloWorld {
+	
+	private String name;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+}
